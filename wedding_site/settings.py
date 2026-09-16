cacde3f40171b4648341487e4c860a296881fba5
batyrov-beka <baatyrovbajys@gmail.com>
2026-09-16 21:18:15 +0600

    Fix staticfiles and connect Neon DB

--- wedding_site/settings.py
+++ wedding_site/settings.py
@@ -18,18 +18,17 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
-# Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.staticfiles',
-    'cloudinary_storage',
-    'cloudinary',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'invitations',
     'import_export',
 ]
